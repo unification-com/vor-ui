5cfda1b77ca5bece1332cacf91d2f5e7d4302436
Shing Ho Tan <talenteyedev@gmail.com>
2021-06-14 09:20:57 -0400

    add Distribution contract

---
+++ truffle-config.js
@@ -0,0 +1,70 @@
+require("dotenv").config()
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {    
+    // ganache-cli
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "696969",       // Any network (default: none)
+    },
+    // truffle develop console
+    develop: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "696969",
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: '0.6.12',
+      settings: {
+          optimizer: {
+              enabled: true,
+              runs: 200,
+          }
+      }
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  //
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+};
