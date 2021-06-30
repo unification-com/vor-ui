3f9e6c0e79cdd72b9ebb8ff9644d0aeb604e7e3a
Codegnosis <paul@codegnosis.co.uk>
2021-06-30 09:21:10 +0100

    build commands for ui & service

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const { exit } = require('process');
+const path = require("path")
 
 module.exports = {
-    paths: function (paths, env) {
-        paths.appPath = path.resolve(__dirname, 'front');
-        paths.appIndexJs = path.resolve(__dirname, 'front/src/index.js');
-        paths.appSrc = path.resolve(__dirname, 'front/src');
-        paths.appPublic = path.resolve(__dirname, 'front/public');
-        paths.appHtml = path.resolve(__dirname, 'front/public/index.html');
-        paths.publicUrlOrPath = "/";
-        return paths;
-    },
+  paths(paths, env) {
+    paths.appPath = path.resolve(__dirname, "front")
+    paths.appIndexJs = path.resolve(__dirname, "front/src/index.js")
+    paths.appSrc = path.resolve(__dirname, "front/src")
+    paths.appPublic = path.resolve(__dirname, "front/public")
+    paths.appHtml = path.resolve(__dirname, "front/public/index.html")
+    paths.appBuild = path.resolve(__dirname, "build/front")
+    paths.publicUrlOrPath = "/"
+    return paths
+  },
 }
